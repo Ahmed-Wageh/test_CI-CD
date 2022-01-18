402ee0769387b6bbf31c3e0f22ed6470bf64ddd0
Ahmed-Wageh <ahmed.wageh@brightskiesinc.com>
2022-01-18 12:44:57 +0200

    Update UnitTestScript.py

--- UnitTestScript.py
+++ UnitTestScript.py
@@ -1,6 +1,13 @@
 import os
 
-if os.system(r"G:\test_CI-CI\a.exe") == 0:
+exitCode = 0
+
+print("Testing .........")
+
+if os.system(r"G:\test_CI-CD\a.exe") == 0:
     print("PASSED\n")
 else:
+    exitCode = 1
     print("FAILED\n")
+    
+exit(exitCode)
